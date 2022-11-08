cf3a16a47e5006d92cb6749aea9598fd5acc1afe
Tufan Ionut-Petrisor <117408976+TufanIonut@users.noreply.github.com>
2022-11-08 15:14:30 +0200

    Add files via upload

---
+++ SchimbValutar/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace SchimbValutar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ceSeObtine = new System.Windows.Forms.ComboBox();
+            this.DeSchimbat = new System.Windows.Forms.ComboBox();
+            this.tbx2 = new System.Windows.Forms.TextBox();
+            this.tbx1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(203)))), ((int)(((byte)(255)))));
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(132, 480);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(67)))), ((int)(((byte)(85)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Bold);
+            this.button1.Location = new System.Drawing.Point(8, 285);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 30);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Convert";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(67)))), ((int)(((byte)(85)))));
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(650, 37);
+            this.panel2.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(85)))), ((int)(((byte)(203)))), ((int)(((byte)(200)))));
+            this.label1.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(16, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "WELCOME";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Rockwell", 16F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(138, 5);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(237, 27);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Currency Converter ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Rockwell", 14F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(466, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(204, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Moneda schimbata :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Rockwell", 14F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(139, 71);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(150, 23);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Moneda mea : ";
+            // 
+            // ceSeObtine
+            // 
+            this.ceSeObtine.Font = new System.Drawing.Font("Rockwell", 14.25F, System.Drawing.FontStyle.Bold);
+            this.ceSeObtine.FormattingEnabled = true;
+            this.ceSeObtine.Location = new System.Drawing.Point(470, 97);
+            this.ceSeObtine.Name = "ceSeObtine";
+            this.ceSeObtine.Size = new System.Drawing.Size(253, 31);
+            this.ceSeObtine.TabIndex = 4;
+            // 
+            // DeSchimbat
+            // 
+            this.DeSchimbat.Font = new System.Drawing.Font("Rockwell", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DeSchimbat.FormattingEnabled = true;
+            this.DeSchimbat.Location = new System.Drawing.Point(143, 97);
+            this.DeSchimbat.Name = "DeSchimbat";
+            this.DeSchimbat.Size = new System.Drawing.Size(253, 31);
+            this.DeSchimbat.TabIndex = 5;
+            // 
+            // tbx2
+            // 
+            this.tbx2.Font = new System.Drawing.Font("Rockwell", 14.25F, System.Drawing.FontStyle.Bold);
+            this.tbx2.Location = new System.Drawing.Point(470, 153);
+            this.tbx2.Name = "tbx2";
+            this.tbx2.Size = new System.Drawing.Size(252, 30);
+            this.tbx2.TabIndex = 6;
+            // 
+            // tbx1
+            // 
+            this.tbx1.Font = new System.Drawing.Font("Rockwell", 14.25F, System.Drawing.FontStyle.Bold);
+            this.tbx1.Location = new System.Drawing.Point(143, 153);
+            this.tbx1.Name = "tbx1";
+            this.tbx1.Size = new System.Drawing.Size(253, 30);
+            this.tbx1.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Bold);
+            this.label5.Location = new System.Drawing.Point(139, 131);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 19);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Suma :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Bold);
+            this.label6.Location = new System.Drawing.Point(467, 131);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(63, 19);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Suma :";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(143, 285);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(236, 20);
+            this.dateTimePicker1.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(134)))), ((int)(((byte)(133)))), ((int)(((byte)(175)))));
+            this.ClientSize = new System.Drawing.Size(772, 336);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tbx1);
+            this.Controls.Add(this.tbx2);
+            this.Controls.Add(this.DeSchimbat);
+            this.Controls.Add(this.ceSeObtine);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Money changer";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox ceSeObtine;
+        private System.Windows.Forms.ComboBox DeSchimbat;
+        private System.Windows.Forms.TextBox tbx2;
+        private System.Windows.Forms.TextBox tbx1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ SchimbValutar/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SchimbValutar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            DeSchimbat.Items.Add("US DOLLARS"); 
+            DeSchimbat.Items.Add("EURO");
+            DeSchimbat.Items.Add("BRITISH POUND");
+            DeSchimbat.Items.Add("LEI");
+            DeSchimbat.Items.Add("CANADIAN DOLLARS");
+
+
+            //---------------------------------
+
+
+            ceSeObtine.Items.Add("US DOLLARS");
+            ceSeObtine.Items.Add("EURO");
+            ceSeObtine.Items.Add("BRITISH POUND");
+            ceSeObtine.Items.Add("CANADIAN DOLLARS");
+            ceSeObtine.Items.Add("LEI");
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (DeSchimbat.Text == "US DOLLARS" && ceSeObtine.Text == "EURO")
+            {
+                tbx2.Text = (double.Parse(tbx1.Text) * (0.89882)).ToString();
+            }
+            if (DeSchimbat.Text == "US DOLLARS" && ceSeObtine.Text == "BRITISH POUND")
+            {
+                tbx2.Text = (double.Parse(tbx1.Text) * (0.76554)).ToString();
+            }
+            if (DeSchimbat.Text == "US DOLLARS" && ceSeObtine.Text == "LEI")
+            {
+                tbx2.Text = (double.Parse(tbx1.Text) * (4.28175)).ToString();
+            }
+            if (DeSchimbat.Text == "US DOLLARS" && ceSeObtine.Text == "CANADIAN DOLLARS")
+            {
+                tbx2.Text = (double.Parse(tbx1.Text) * (1.31188)).ToString();
+            }
+            if (DeSchimbat.Text == "EURO" && ceSeObtine.Text == "US DOLLARS")
+            {
+                tbx2.Text = (double.Parse(tbx1.Text) * (1.11243)).ToString();
+            }
+            if (DeSchimbat.Text == "EURO" && ceSeObtine.Text == "CANADIAN DOLLARS")
+            {
+                tbx2.Text = (double.Parse(tbx1.Text) * (1.45938)).ToString();
+            }
+            if (DeSchimbat.Text == "EURO" && ceSeObtine.Text == "BRITISH POUND")
+            {
+                tbx2.Text = (double.Parse(tbx1.Text) * (0.85164)).ToString();
+            }
+            if (DeSchimbat.Text == "EURO" && ceSeObtine.Text == "LEI")
+            {
+                tbx2.Text = (double.Parse(tbx1.Text) * (4.76317)).ToString();
+            }
+            if (DeSchimbat.Text == "BRITISH POUND" && ceSeObtine.Text == "US DOLLARS")
+            {
+                tbx2.Text = (double.Parse(tbx1.Text) * (1.30606)).ToString();
+            }
+            if (DeSchimbat.Text == "BRITISH POUND" && ceSeObtine.Text == "LEI")
+            {
+                tbx2.Text = (double.Parse(tbx1.Text) * (5.59221)).ToString();
+            }
+            if (DeSchimbat.Text == "BRITISH POUND" && ceSeObtine.Text == "EURO")
+            {
+                tbx2.Text = (double.Parse(tbx1.Text) * (1.17394)).ToString();         
+            } 
+            if (DeSchimbat.Text == "BRITISH POUND" && ceSeObtine.Text == "CANADIAN DOLLARS")
+                {
+                    tbx2.Text = (double.Parse(tbx1.Text) * (1.71339)).ToString();
+                }
+            if (DeSchimbat.Text == "LEI" && ceSeObtine.Text == "EURO")
+            {
+                tbx2.Text = (double.Parse(tbx1.Text) * (0.20946)).ToString();
+            }
+            if (DeSchimbat.Text == "LEI" && ceSeObtine.Text == "US DOLLARS")
+            {
+                tbx2.Text = (double.Parse(tbx1.Text) * (0.23304)).ToString();
+            }
+            if (DeSchimbat.Text == "LEI" && ceSeObtine.Text == "CANADIAN DOLLARS")
+            {
+                tbx2.Text = (double.Parse(tbx1.Text) * (0.30572)).ToString();
+            }
+            if (DeSchimbat.Text == "LEI" && ceSeObtine.Text == "BRITISH POUND")
+            {
+                tbx2.Text = (double.Parse(tbx1.Text) * (0.17840)).ToString();
+            }
+            if (DeSchimbat.Text == "CANADIAN DOLLARS" && ceSeObtine.Text == "BRITISH POUND")
+            {
+                tbx2.Text = (double.Parse(tbx1.Text) * (0.58347)).ToString();
+            }
+            if (DeSchimbat.Text == "CANADIAN DOLLARS" && ceSeObtine.Text == "US DOLLARS")
+            {
+                tbx2.Text = (double.Parse(tbx1.Text) * (0.76216)).ToString();
+            }
+            if (DeSchimbat.Text == "CANADIAN DOLLARS" && ceSeObtine.Text == "EURO")
+            {
+                tbx2.Text = (double.Parse(tbx1.Text) * (0.68504)).ToString();
+            }
+            if (DeSchimbat.Text == "CANADIAN DOLLARS" && ceSeObtine.Text == "LEI")
+            {
+                tbx2.Text = (double.Parse(tbx1.Text) * (3.26337)).ToString();
+            }
+            if (DeSchimbat.Text==ceSeObtine.Text)
+            {
+                tbx2.Text = tbx1.Text;
+            }
+        }
+
+     
+    }
+}
